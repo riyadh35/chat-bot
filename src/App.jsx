ae2d34b8989b342e598acbb40454d85d56bdc22e
riyadh35 <mdriyadhossain356@gmail.com>
2023-10-23 02:32:48 +0600

    react chat bot

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,71 @@
-import { useState } from "react";
 import "./App.css";
+import Chatbot from "react-chatbot-kit";
+import { Segment } from "semantic-ui-react";
+import ActionProvider from "./chatbot/ActionProvider";
+import config from "./bot/config";
+import ActionProvider from "./bot/ActionProvider";
+import MessageParser from "./bot/MessageParser";
 
 function App() {
-  return <></>;
+  const steps = [
+    {
+      id: "welcome",
+      message: "Hello! How can I help you today?",
+      trigger: "Ask Name",
+    },
+    {
+      id: "Ask Name",
+      message: "What is your name?",
+      trigger: "Ask Age",
+    },
+    {
+      id: "waiting",
+      user: true,
+      trigger: "Name",
+    },
+    {
+      id: "name",
+      message: "Hi {previousvalue} , please select your issue",
+      trigger: "issues",
+    },
+    {
+      name: "issues",
+      options: [
+        {
+          value: "React",
+          label: "React",
+          trigger: "React",
+        },
+        {
+          value: "Angular",
+          label: "Angular",
+          trigger: "Angular",
+        },
+      ],
+    },
+    {
+      id: "React",
+      message: "Thanks for telling your issues.",
+      end: true,
+    },
+    {
+      id: "Angular",
+      message: "Thanks for telling your issues.",
+      end: true,
+    },
+  ];
+  return (
+    <>
+      <Segment floated="right">
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+          steps={steps}
+        />
+      </Segment>
+    </>
+  );
 }
 
 export default App;
